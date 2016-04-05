2162807c3e5e644a2749000cc965423a32d95b15
Andrew Fecheyr <andrew@bedesign.be>
2016-04-05 10:06:50 +0200

    Fix region detection to run when page is ready

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
+//
 // Region detection
+//
+
 function changeRegion(region) {
   if (window.region != region) {
     $(document).trigger('changeRegion', region);
@@ -10,18 +13,11 @@ function changeRegion(region) {
 $(document).on('changeRegion', function(event, region) {
   if (region == "US") {
     regionImage = "/images/us.png";
-  } else {
-    regionImage = "/images/eu.png";
-  }
-  $("#regionSelector").attr("src", regionImage);
-})
-
-$(document).on('changeRegion', function(event, region) {
-  if (region == "US") {
     $("[data-us-content]").each(function(idx, element) {
       $(element).html($(element).data("us-content"))
     })
   } else {
+    regionImage = "/images/eu.png";
     $("[data-eu-content]").each(function(idx, element) {
       $(element).html($(element).data("eu-content"))
     })
@@ -29,7 +25,10 @@ $(document).on('changeRegion', function(event, region) {
   $("#regionSelector").attr("src", regionImage);
 })
 
-changeRegion("EU"); // Default to EU
+$(document).on("ready", function() {
+  changeRegion("EU"); // Default to EU
+})
+
 function geocallback(data) {
   country_code = data.country_code;
   // US and CA will get the US localised version
@@ -42,7 +41,7 @@ function geocallback(data) {
 // $.get("https://freegeoip.net/json/8.8.8.8?callback=geocallback")
 $.get("https://freegeoip.net/json/?callback=geocallback")
 
-$("#regionSelector").on("click", function() {
+$(document).on("click", "#regionSelector", function() {
   if (window.region == "US") {
     newRegion = "EU";
   } else {
