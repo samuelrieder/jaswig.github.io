ba93ecef3ee134240eb56be5da6677261d29c6b6
Andrew Fecheyr <andrew@bedesign.be>
2016-04-21 16:51:28 +0200

    Fetch photos from flickr

--- js/instagram.js
+++ js/instagram.js
@@ -3,15 +3,7 @@ Vue.config.delimiters = ['{', '}'];
 var instagram = new Vue({
   el: '#vue-instagram',
   data: {
-    title: 'http://placehold.it/10x10',
-    photos: [
-      'https://scontent-amt2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/11379302_968060666618228_2045381212_n.jpg?ig_cache_key=MTIxMTA4NTU0NTA0NjQzNzk5Nw%3D%3D.2',
-      'https://scontent-amt2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12424936_177501069307829_2010337847_n.jpg?ig_cache_key=MTIxNDY3ODcyODA1NzcxMjgxMw%3D%3D.2',
-      'https://scontent-amt2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/11930955_135967876761681_328026501_n.jpg?ig_cache_key=MTExMTA1NzkzMTM1MTc4MjMwNg%3D%3D.2',
-      'https://scontent-amt2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12224277_883923038355418_167651062_n.jpg?ig_cache_key=MTEyMjY0NTkxODgwODE3NTM4Ng%3D%3D.2',
-      'https://scontent-amt2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/c180.0.720.720/12256793_559801474176550_1229734148_n.jpg?ig_cache_key=MTEzNDMxNDAyODE5MTYwOTc4NQ%3D%3D.2.c',
-      'https://scontent-amt2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/c179.0.721.721/12547678_985671571515577_128322351_n.jpg?ig_cache_key=MTE3MTI0ODU2MzY3NjE0ODEwOA%3D%3D.2.c'
-    ]
+    photos: false,
   },
 
   methods: {
@@ -22,10 +14,28 @@ var instagram = new Vue({
 
   created: function () {
     setInterval(function(){
-      this.photos.$set(
-        Math.floor(Math.random()*this.photos.length),
-        this.photos[Math.floor(Math.random()*this.photos.length)]
-      );
+      if(this.photos) {
+        this.photos.$set(
+          Math.floor(Math.random()*this.photos.length),
+          this.photos[Math.floor(Math.random()*this.photos.length)]
+        );
+      }
     }.bind(this), 10000);
   }
 })
+
+function flickrPhotoUrl(photo, size) {
+  return "https://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+"_"+size+".jpg"
+}
+
+function jsonFlickrApi(response) {
+  photos = response.photos.photo
+  urls = []
+  for (var i = 0; i < photos.length; i++) {
+    urls.push(flickrPhotoUrl(photos[i], "z"));
+  }
+  instagram.photos = urls
+}
+
+// Fetch fotos
+$.get("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&user_id=11805358@N03&api_key=f0b84fba1c00631410b85b90720f52ba&format=json")
