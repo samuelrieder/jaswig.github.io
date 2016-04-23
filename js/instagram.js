afb7fb9a7629e2b5da3244ee433829f822c58506
Andrew Fecheyr <andrew@bedesign.be>
2016-04-23 13:37:50 +0200

    JSHint warnings

--- js/instagram.js
+++ js/instagram.js
@@ -8,7 +8,7 @@ var instagram = new Vue({
 
   methods: {
     getRandomPhoto: function(){
-      return Math.floor(Math.random()*this.photos.length)
+      return Math.floor(Math.random()*this.photos.length);
     }
   },
 
@@ -22,20 +22,20 @@ var instagram = new Vue({
       }
     }.bind(this), 10000);
   }
-})
+});
 
 function flickrPhotoUrl(photo, size) {
-  return "https://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+"_"+size+".jpg"
+  return "https://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+"_"+size+".jpg";
 }
 
 function jsonFlickrApi(response) {
-  photos = response.photos.photo
-  urls = []
+  photos = response.photos.photo;
+  urls = [];
   for (var i = 0; i < photos.length; i++) {
     urls.push(flickrPhotoUrl(photos[i], "z"));
   }
-  instagram.photos = urls
+  instagram.photos = urls;
 }
 
 // Fetch fotos
-$.get("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&user_id=140453763@N07&api_key=f0b84fba1c00631410b85b90720f52ba&format=json")
+$.get("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&user_id=140453763@N07&api_key=f0b84fba1c00631410b85b90720f52ba&format=json");
