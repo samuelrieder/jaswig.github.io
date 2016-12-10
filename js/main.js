193e55de6f49946fcc31778c14465ff4f921b430
Krekotun <a.krekotun@gmail.com>
2016-12-10 15:22:22 +0200

    cookies popup

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@
 # Dont delete this line. This main JS file needs this front matter
 ---
 {% include_relative vendor/jquery.matchHeight.min.js %}
+{% include_relative vendor/js.cookie.js %}
 {% include_relative instagram.js %}
 {% include_relative scrolldesk.js %}
 {% include_relative region.js %}
 {% include_relative videomodal.js %}
+{% include_relative cookies-warning.js %}
 
 $(document).on('ready', function() {
   $('.order-card-text').matchHeight({byRow: true});
+  var cookiesWarning = new window.CookiesWarning()
 });
 
 $(document).on('click', '.hero .cta, .mobile-content .cta', function(event) {
